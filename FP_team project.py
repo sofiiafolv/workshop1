22da1bc6ef5276b14c10216566f7e9dfdb316120
Artem Moskovets <93625455+artemka169@users.noreply.github.com>
2021-12-15 12:42:43 +0200

    Add files via upload

--- FP_team project.py
+++ FP_team project.py
@@ -1,9 +1,9 @@
 from idlelib.multicall import r
 
 
-def read_csv(path):
+def read_csv(path_file):
     """
-    :param path: path to csv file
+    :param path_file: path to csv file
     :return: list of lists, consists of 5 elements:
     0 - name of task
     1 - plasce
@@ -12,7 +12,7 @@ def read_csv(path):
     4 - priority
     """
     all_list = []
-    with open(path, 'r', encoding='utf-8') as csv_file:
+    with open(path_file, 'r', encoding='utf-8') as csv_file:
         for line in csv_file:
             line = line.strip()
             line = line.split(',')
@@ -32,15 +32,20 @@ def print_csv(all_list):
     print()
 
 
-def delete_notion(filepath, object):
-    """Accepts a file a delete a done task, which the user enters"""
+def delete_notion(filepath, name_task):
+    """
+    Delete task from csv file
+    :param filepath:
+    :param name_task:
+    :return:
+    """
     with open(filepath, mode='r', encoding='utf-8') as file:
         data = file.readlines()
     for i in range(len(data)):
         data[i] = data[i].strip("\n")
         data[i] = data[i].split(',')
     for i in data:
-        if object in i:
+        if name_task in i:
             data.pop(data.index(i))
     with open(filepath, mode='w', encoding='utf-8') as file:
         for item in data:
@@ -68,13 +73,52 @@ def tasks_today(list_of_tasks):
     print()
 
 
-def write_csv(path, new_task):
+def write_csv(path_file, new_task):
     """
     :param new_task: what to write in csv file
-    :param path: path to csv file
+    :param path_file: path to csv file
     :return: nothing
      writes a new line (task) to csv file
     """
-    with open(path, 'a', encoding='utf-8') as csv_file:
-        csv_file.write("\n")
-        csv_file.write(new_task)
+    with open(path_file, 'a', encoding='utf-8') as csv_file:
+        csv_file.write('\n' + new_task)
+
+
+def add_tasks():
+    """Asks information about task and returns it in csv format"""
+    task = input('Write a task: ')
+    location = input('Write a location: ')
+    collaborators = input('Write your coworkers: ')
+    date = input('Write the date by which the task must be completed in format dd.mm.yyyy: ')
+    priority = input('Write a priority from 1 to the number of the last task: ')
+    lst = [task,location,collaborators,date,priority]
+    return ','.join(lst)
+
+if __name__ == '__main__':
+    print('enter your path to csv file with tasks')
+    path = input()
+    while True:
+        print('Enter 1 if you want to add task')
+        print('Enter 2 if you want to delete task')
+        print('Enter 3 if you want to see today task')
+        print('Enter 4 to see all task, sorted by priority')
+        print('Enter exit if you want to exit')
+        action = input()
+        if action == '1':
+            print("What task do you want to add ?")
+            task = add_tasks()
+            write_csv(path, task)
+        elif action == '2':
+            print("What task do you want to delete ?")
+            task = input()
+            delete_notion(path, task)
+        elif action == '3':
+            print("Do you want to see today tasks ?")
+            tasks_today(read_csv(path))
+        elif action == '4':
+            print_csv(read_csv(path))
+        elif action == "exit":
+            print('thanks for using, bye')
+            break
+        else:
+            print('wrong input, repeat one more time')
